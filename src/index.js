d3a09cbc3af196a539cb5f4d7a691b61256094df
Saqib <zaman342003@gmail.com>
2022-04-30 05:18:34 +0500

    Todo app is ready using redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import ReactDom from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -15,3 +24,29 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// // import ReactDOM from 'react-dom/client';
+// import ReactDom from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import { Provider } from react-redux;
+// import { BrowserRouter } from "react-router-dom"
+// import reportWebVitals from './reportWebVitals';
+// import store from './redux/store';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     <Provider store={store}>
+//     <App />
+//     </Provider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
---
+++ src/pages/AddUser.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+// import { userAdded } from './../redux/actionType';
+import { addUser } from "../redux/actions";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      marginTop: 100,
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "45ch",
+      },
+    }, 
+  }));
+
+const AddUser = () => {
+  const classes = useStyles();
+
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    contact: "",
+    address: "",
+  });
+  const [error, setError] = useState("");
+
+
+  let navigate = useNavigate();
+  let dispatch = useDispatch();
+  const { name, email, contact, address } = state;
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !address || !email || !contact) {
+      setError("Please input all input Field");
+    } else {
+      dispatch(addUser(state));
+      navigate("/");
+      setError("");
+    }
+  };
+
+  return (
+    <div>
+      <Button
+        style={{ width: "100px", marginTop: "20px" }}
+        variant="contained"
+        color="secondary"
+        onClick={() => navigate("/")}
+      >
+        Go Back
+      </Button>
+      <h2>Add User</h2>
+      {error && <h3 style={{ color: "red" }}>{error}</h3>}
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          id="standard-basic"
+          label="Name"
+          value={name}
+          name="name"
+          type="text"
+          onChange={handleInputChange}
+        />
+        <br />
+        <TextField
+          id="standard-basic"
+          label="Email"
+          name="email"
+          value={email}
+          type="email"
+          onChange={handleInputChange}
+        />
+        <br />
+        <TextField
+          id="standard-basic"
+          label="Contact"
+          value={contact}
+          name="contact"
+          type="number"
+          onChange={handleInputChange}
+        />
+        <br />
+        <TextField
+          id="standard-basic"
+          label="Address"
+          value={address}
+          name="address"
+          type="text"
+          onChange={handleInputChange}
+        />
+        <br />
+        <Button
+          styles={{ width: "100px" }}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onChange={handleInputChange}
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
+
+
+
